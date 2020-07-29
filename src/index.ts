cd2282d454e64f96bb8ff863b3174a1825ed2239
Tom Bocklisch <tom@rasa.com>
2020-07-29 18:21:30 +0200

    don't fail on error, just sending to many emails

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import * as core from '@actions/core'
 import main from './main'
 
 main(core, github).catch((error: Error) => {
-    core.setFailed(error.message)
+    console.log("Failed to merge!", error.message)
+    console.log(error)
 })
